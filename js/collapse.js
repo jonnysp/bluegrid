1f478b9d0168515f0e38b619cbcfd20e42a66a81
jonnysp <jonnyspitzner@googlemail.com>
2012-11-07 16:51:54 +0100

    some changes

--- js/collapse.js
+++ js/collapse.js
@@ -8,6 +8,24 @@
     	var data = $(this).data();
 		var data_target = $(data['target']);
 		var data_group = data['group'];
+		
+
+		$(this).click(function() {
+			if (data_target){
+				
+				if ($(data_target).hasClass('in')){
+					$(this).removeClass('active');
+					$(data_target).slideUp();
+					$(data_target).removeClass('in').addClass('out');
+				}else{
+					$(this).addClass('active');
+					$(data_target).slideDown();
+					$(data_target).removeClass('out').addClass('in');
+				}
+				
+			}  
+		});
+
 
 		
 		
