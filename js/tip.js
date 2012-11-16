a1ab8bc4f73d9e7b49511c5e2841ea12ca0bc30d
jonnysp <jonnyspitzner@googlemail.com>
2012-11-16 16:54:38 +0100

    tests new plugin system

--- js/tip.js
+++ js/tip.js
@@ -1,77 +1,112 @@
-(function( $ ){
+(function($){
 
-  $.fn.tip = function(options) {  
-    var settings = $.extend( {}, options);
-		 
-    return this.each(function() {        
-		
-		var data = $(this).data();
-		var data_content = $(data['content']).html() ? $(data['content']).html() : data['content'];
-		var data_class = data['class'] ? data['class'] : '';
-		var data_pos = data['pos'] ? data['pos'] : 'top';
-		var tip = $('<div class="tip" />')
-			.addClass(data_class)
-			.addClass(data_pos)
-			.html(data_content).hide();
-		
-		$(this).hover(
-		  function () {
-			  
-			if (data_content && data_content != '') { 
-			 
-				$(tip).appendTo('body');
-				
-				var tip_width = $(tip).outerWidth(true);
-				var tip_height = $(tip).outerHeight(true);
-				
-				var elmpos = $(this).position();
-				var elmwidth = $(this).outerWidth(true);
-				var elmheight = $(this).outerHeight(true);
-				
-				var tp = {'top':0,'left':0};
-				switch(data_pos){
-					case 'top':
-						tp = {'top':elmpos.top - tip_height,'left':(elmpos.left + (elmwidth / 2)) - (tip_width / 2) };
-						break	
-					case 'bottom':
-						tp = {'top':elmpos.top + elmheight,'left':(elmpos.left + (elmwidth / 2)) - (tip_width / 2) };
-						break
-					case 'left':
-						tp = {'top':(elmpos.top + (elmheight / 2)) - (tip_height / 2),'left':elmpos.left - tip_width};
-						break
-					case 'right':
-						tp = {'top':(elmpos.top + (elmheight / 2)) - (tip_height / 2),'left':elmpos.left + elmwidth};
-						break
-				}
-				
-			 
-				  $(tip).css(tp).addClass('in').removeClass('out').show();
-			 };
-			
-		  }, 
-		  function () {	
-		  
-		  	$(tip).addClass('out').removeClass('in');
-		  
-		 	var s = document.body.style,
-			supportsTransitions =   'WebkitTransition' in s || 'MozTransition' in s || 'msTransition' in s || 'OTransition' in s || 'Transition' in s;
-		  	
-			if (supportsTransitions){
-				$(tip).one("transitionend webkitTransitionEnd otransitionend", function(){ 
-					$(this).detach();
-				});
-			}else{
-				$(tip).remove();
-			}
-			
-			
-			
-		   }
-		);
+	function Tip(el) {
+		this.$el = $(el);
+	}
 
-    });
+	Tip.prototype = {
+		init: function() {
+			var _this = this;
+			$(_this.$el).hover(
+		  		function () {
+			  		_this.show();
+			  });
+		},
+		show: function(){
+			alert("");
+		},
+		hide: function(){
+			
+		}
+	};
 
-  };
+	$.fn.tip = function() {
+		if(this.length) {
+			this.each(function() {
+				var rev = new Tip(this);
+				rev.init();
+				$(this).data('tip', rev);
+			});
+		}
+	};
+
+
+
+
+
+//  $.fn.tip = function(options) {  
+//    var settings = $.extend( {}, options);
+//		 
+//    return this.each(function() {        
+//		
+//		var data = $(this).data();
+//		var data_content = $(data['content']).html() ? $(data['content']).html() : data['content'];
+//		var data_class = data['class'] ? data['class'] : '';
+//		var data_pos = data['pos'] ? data['pos'] : 'top';
+//		var tip = $('<div class="tip" />')
+//			.addClass(data_class)
+//			.addClass(data_pos)
+//			.html(data_content).hide();
+//		
+//		$(this).hover(
+//		  function () {
+//			  
+//			if (data_content && data_content != '') { 
+//			 
+//				$(tip).appendTo('body');
+//				
+//				var tip_width = $(tip).outerWidth(true);
+//				var tip_height = $(tip).outerHeight(true);
+//				
+//				var elmpos = $(this).position();
+//				var elmwidth = $(this).outerWidth(true);
+//				var elmheight = $(this).outerHeight(true);
+//				
+//				var tp = {'top':0,'left':0};
+//				switch(data_pos){
+//					case 'top':
+//						tp = {'top':elmpos.top - tip_height,'left':(elmpos.left + (elmwidth / 2)) - (tip_width / 2) };
+//						break	
+//					case 'bottom':
+//						tp = {'top':elmpos.top + elmheight,'left':(elmpos.left + (elmwidth / 2)) - (tip_width / 2) };
+//						break
+//					case 'left':
+//						tp = {'top':(elmpos.top + (elmheight / 2)) - (tip_height / 2),'left':elmpos.left - tip_width};
+//						break
+//					case 'right':
+//						tp = {'top':(elmpos.top + (elmheight / 2)) - (tip_height / 2),'left':elmpos.left + elmwidth};
+//						break
+//				}
+//				
+//			 
+//				  $(tip).css(tp).addClass('in').removeClass('out').show();
+//			 };
+//			
+//		  }, 
+//		  function () {	
+//		  
+//		  	$(tip).addClass('out').removeClass('in');
+//		  
+//		 	var s = document.body.style,
+//			supportsTransitions =   'WebkitTransition' in s || 'MozTransition' in s || 'msTransition' in s || 'OTransition' in s || 'Transition' in s;
+//		  	
+//			if (supportsTransitions){
+//				$(tip).one("transitionend webkitTransitionEnd otransitionend", function(){ 
+//					$(this).detach();
+//				});
+//			}else{
+//				$(tip).remove();
+//			}
+//			
+//			
+//			
+//		   }
+//		);
+//
+//    });
+//
+//  };
+
 })( jQuery );
 
 $(document).ready(function(){
