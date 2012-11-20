62fa47c24d0c0872c3c4e4859dd00a50e7accde5
Jonny Spitzner <jonnyspitzner@googlemail.com>
2012-11-20 20:59:24 +0100

    modal prototype

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,13 @@
 		function Modal(element) {
 			this.$element = $(element);
 			this.$target =  $(this.$element.data('target')).length > 0 ? $(this.$element.data('target')) : $(this.$element.data('target'));
+			this.$class = this.$element.data('class') ? this.$element.data('class') : '';
+			this.$transition = this.$element.data('transition') ? this.$element.data('transition') : '';
 			this.$isclose = this.$element.data('close') ? true : false;
-			
 			if (this.$isclose === true){
 				this.$target = $(this.$element.data('close')).length > 0 ? $(this.$element.data('close')) : $(this.$element.data('close'));
-			}else{
-				this.$backdrop = $('<div />').addClass('modal-backdrop').addClass('out');
-				this.$visible = false;
 			}
-
-
+			this.$target.data('backdrop', $('<div />').addClass('modal-backdrop').addClass('out') );
 		};
 
 
@@ -20,113 +17,54 @@
 	Modal.prototype = {
 		
 			init: function(self) {
+					
+					self.$target.addClass(self.$class).addClass(self.$transition);
 					if (self.$isclose === true){
-							
+						self.$element.click(function(){
+							self.hide()
+						})
 					}else{
 						self.$element.click(function(){
 							self.show()
 						});
 					}
+					this.$target.data('backdrop').click(function(){
+							self.hide()
+					});	
 					
 			},
 			
 			show: function() {
-				alert("show");
+				
+					this.$target.data('backdrop').appendTo('body').addClass('in').removeClass('out');
+					this.$target.removeClass('out').addClass('in');
+					
 			},
 			
 			hide: function() {
-				alert("hide");
+			
+					this.$target.data('backdrop').removeClass('in').addClass('out');
+				    this.$target.removeClass('in').addClass('out');
+					
 			}
 
 	}
 
 
-		$.fn.modal = function() {
-			  return this.each(function(){
-					var element = $(this);
-					if (element.data('modal')){ 
-						return element.data('modal') 
-					}else{
-						var self = new Modal(this);
-						self.init(self);
-						element.data('modal', self);
-					};
-			  });
-		};
+	  $.fn.modal = function() {
+			return this.each(function(){
+				  var element = $(this);
+				  if (element.data('modal')){ 
+					  return element.data('modal') 
+				  }else{
+					  var self = new Modal(this);
+					  self.init(self);
+					  element.data('modal', self);
+				  };
+			});
+	  };
 
 
-
-
-//  $.fn.modal = function(options) {  
-//    var settings = $.extend( {}, options);
-//	
-//	var modalclose = function(that) {
-//			if ($(that).data('close') ){
-//				$($(that).data('close')).removeClass('in').addClass('out');
-//				if ($(that).data('target')){
-//					$($($(that).data('target'))).each(function(index, element) {
-//                        $(this).removeClass('active');
-//						var backd = $($(this).data('backdrop'));
-//						$(backd).removeClass('in').addClass('out');
-//                    });
-//				}
-//			}else{
-//				if ($(that).data('target')){
-//					$(that).removeClass('active');
-//					$($(that).data('target')).removeClass('in').addClass('out');
-//					$($(that).data('backdrop')).removeClass('in').addClass('out');
-//				}
-//			}
-//	} ;
-//	
-//	var modalshow = function(that){
-//		  if ($(that).data('target')){
-//				if (!$($(that).data('target')).hasClass('in')){
-//					$(this).addClass('active');
-//					$($(that).data('backdrop')).appendTo('body').addClass('in').removeClass('out');
-//					$($(that).data('target')).removeClass('out').addClass('in');
-//				}
-//		   }  
-//	};
-// 
-//    return this.each(function() {        
-//		
-//		var that = $(this);
-//    	var data = $(that).data();
-//		var data_target = data['target'];
-//		var data_transition = data['transition'];
-//		var data_class = data['class'];
-//		var data_close = data['close'];
-//		
-//
-//		if (data_target && data_transition){
-//			$(data_target).addClass(data_transition);
-//		}
-//		if (data_target && data_class){
-//			$(data_target).addClass(data_class);
-//		}
-//
-//
-//		if (data_close){
-//			$(that).data('target',$('*[data-target="'+ data_close +'"]')) ;
-//			$(that).click(function() {
-//				modalclose(that);
-//			});
-//		}else{
-//			var backdrop =  $('<div class="modal-backdrop" />').addClass('out');
-//			$(that).data('backdrop',backdrop) ;
-//			$(backdrop).click(function() {
-//				modalclose(that);
-//			})
-//			$(that).click(function() {
-//				modalshow(that);
-//			});
-//		}
-//
-//    });
-//	
-//  };
-  
 })( jQuery );
 
 $(document).ready(function(){
