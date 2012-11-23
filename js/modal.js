2166edbb1a335a8bce4c7698fbee4009ee57471d
jonnysp <jonnyspitzner@googlemail.com>
2012-11-23 16:43:57 +0100

    some typo, ribbon

--- js/modal.js
+++ js/modal.js
@@ -4,34 +4,23 @@
 			this.$element = $(element);
 			this.$target =  $(this.$element.data('target')).length > 0 ? $(this.$element.data('target')) : $(this.$element.data('target'));
 			this.$close =   $(this.$element.data('close')).length > 0 ? $(this.$element.data('close')) : $(this.$element.data('close'));
-
 			if (this.$target.length > 0){
 				this.$target.data('backdrop', $('<div />').addClass('modal-backdrop').addClass('out') );
 			}
 
-			this.$class = this.$element.data('class') ? this.$element.data('class') : '';
-			this.$transition = this.$element.data('transition') ? this.$element.data('transition') : '';
-			
 		};
-
-
 
 	Modal.prototype = {
 		
 			init: function(self) {
-					
-					self.$target.addClass(self.$class).addClass(self.$transition);
-
 						self.$element.click(function(){
 							self.show();
 						});
-						
 						if (self.$target.length > 0){
 							self.$target.data('backdrop').click(function(){
-								self.hide();
+								self.hide(self.$target);
 							});	
 						}
-
 			},
 			
 			show: function() {
@@ -40,25 +29,16 @@
 						this.$target.data('backdrop').appendTo('body').addClass('in');
 						this.$target.addClass('in');
 					}
-					this.closer()
+					this.hide(this.$close);
 					
 			},
 			
-			hide: function() {
-					if (this.$target.length > 0){
-						this.$target.data('backdrop').removeClass('in');
-						this.$target.removeClass('in');
+			hide: function(elm) {
+					if (elm.length > 0){
+						elm.data('backdrop').removeClass('in');
+						elm.removeClass('in');
 					}
 				    
-			},
-			
-			closer:function(){
-				
-				if (this.$close.length > 0 && this.$close != this.$target){
-						this.$close.data('backdrop').removeClass('in');
-				    	this.$close.removeClass('in');
-					}
-				
 			}
 
 	}
