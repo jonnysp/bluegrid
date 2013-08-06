3db8f02b18fc3ab5cf198372df5eda7c295de405
jonnysp <jonnyspitzner@googlemail.com>
2013-08-06 13:40:51 +0200

    use less for build

--- make.cmd
+++ make.cmd
@@ -1,13 +1,17 @@
-node node_modules\recess\bin\recess less\bluegrid.less --compile > css\bluegrid.css
-node node_modules\recess\bin\recess less\bluegrid.less --compile --compress > css\bluegrid.min.css
+@ECHO OFF
+
+cd node
+ECHO "make bluegrid.css"
+node.exe node_modules\less\bin\lessc ..\less\bluegrid.less > ..\css\bluegrid.css
+node.exe node_modules\less\bin\lessc ..\less\bluegrid.less --yui-compress > ..\css\bluegrid.min.css
+cd ..
+ 
+copy css\bluegrid.css _docs\css\bluegrid.css /Y > nul
+copy css\bluegrid.min.css _docs\css\bluegrid.min.css /Y > nul
 
 
-copy css\bluegrid.css _docs\css\bluegrid.css /Y
-copy css\bluegrid.min.css _docs\css\bluegrid.min.css /Y
-
-
-
-copy js\alert.js+js\collapse.js+js\modal.js+js\popover.js+js\tip.js js\bluegrid.js /Y
-copy js\bluegrid.js _docs\js\bluegrid.js /Y
+Echo "make bluegrid.js"
+copy js\alert.js+js\collapse.js+js\modal.js+js\popover.js+js\tip.js js\bluegrid.js /Y > nul
+copy js\bluegrid.js _docs\js\bluegrid.js /Y > nul
 
 pause
